d3124a2cffa4e4e6bffb62f882af27b06669821c
anushkaagarwal15 <raanushka15@gmail.com>
2024-09-09 23:42:45 +0530

    Created the navbar component.

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is new item component
+      </div>
+    )
+  }
+}
+
+export default NewsItem
